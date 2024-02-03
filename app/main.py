921aa72c284af18b3662e096b8226f24802ff95d
iQueryData <iquerydata@outlook.com>
2024-02-03 20:19:56 +0530

    From lap

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# Spark 
+
+# We need Hadoop Cluster to Run Spark -- Seattle Data Guy 
+
+# Python Dag 
+
+# We Can Do fine with just local server 
+
+# Data Ingestion - Spark ( Pyspark ) 
+# Data Processing (ETL)- Pyspark Or PySpark in DataBricks Or Hive 
+# Data Orchestration - Airflow Or DataBricks Or Autosys 
+# Data Report - Tableau
