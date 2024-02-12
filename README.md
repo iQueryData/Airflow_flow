e578622b899eadfe88d8583c3d175d7f26a96f70
iQueryData <iquerydata@outlook.com>
2024-02-12 08:21:46 +0530

    docker compose working

--- README.md
+++ README.md
@@ -2,18 +2,9 @@
 Learning Airflow
 
 
-Spark 
+Docker Run:
 
-We need Hadoop Cluster to Run Spark -- Seattle Data Guy 
-
-Python Dag 
-
-We Can Do fine with just local server 
-
-Data Ingestion - Spark ( Pyspark ) 
-Data Processing (ETL)- Pyspark Or PySpark in DataBricks Or Hive 
-Data Orchestration - Airflow Or DataBricks Or Autosys 
-Data Report - Tableau
+docker-compose -f docker-compose.yml up -d --force-recreate
 
 Spark 
 
--- app/db_connect.py
+++ app/db_connect.py
@@ -1,4 +1,6 @@
 import psycopg2
+import sys
+import requests, json
 
 
 try:
@@ -6,7 +8,7 @@ try:
                             port = "5432",
                           dbname = "dummyDB",
                           user = "postgres",
-                          password = "postgres") #host is the name in Docker-compose files 
+                          password = "postgres")  #host is the name in Docker-compose files & password
     
     cur = conn.cursor()
 
@@ -19,3 +21,40 @@ try:
 
 except Exception as err:
     print(f"Error Connecting to Postgres: {err}")
+
+
+
+# Generate Data for ETL 
+
+def gen_data():
+
+    try:
+
+        api_url = "https://randomuser.me/api/?results=10"
+
+        response = requests.get(api_url).json()
+
+        with open('sample.json', 'w') as api_data:
+            api_data.write(json.dumps(response, indent=4))
+
+        return True
+
+    except Exception as err:
+
+        print(f"Error During API Data Write : {err}")
+        sys.exit(1)
+
+
+# def insert_to_db():
+#     if gen_data():
+#         # Do Database Connection 
+#         while time_out < 10:
+#             time_out = 0 
+#             time_out += 1
+
+#             conn = psycopg2.connect('HOST',
+#                                     'PORT',
+#                                     'DATABASE',
+#                                     'USERNAME',
+#                                     'PASSWORD')
+            
